80b69360ec7ca96193cc282fb306409cee74b4e4
Drew Ipson <drewipson@gmail.com>
2021-01-31 19:44:18 -0700

    Changed package name to data-wrangler

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("./README.md", 'r', encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DW",
+    name="data-wrangler",
     version="0.0.3",
     author="Drew Ipson",
     author_email="drewipson@gmail.com",
