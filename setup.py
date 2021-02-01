75114bb2231f9ea605e1f1d0b5e0671b46322edb
Drew Ipson <drewipson@gmail.com>
2021-01-31 17:54:10 -0700

    Changes to the setup.py file for configuration.

--- setup.py
+++ setup.py
@@ -4,12 +4,19 @@ with open("README.md", 'r', encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DataWranger-data.dr3w-ipso_n",
+    name="DataWrangler-data.dr3w-ipso_n",
     version="0.0.1",
     author="Drew Ipson",
     author_email="drewipson@gmail.com",
     description="A small python package full of useful methods for data cleaning and manipulation.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=""
+    url="https://github.com/drewipson/DW",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
 )
